8086f1e4a4792d8f73c833a8ce83a0d0556939f2
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-07-11 18:47:47 +0530

    added Welcome component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <welcome-component msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Welcome from "@/components/Welcome.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    "welcome-component": Welcome
   }
 };
 </script>
