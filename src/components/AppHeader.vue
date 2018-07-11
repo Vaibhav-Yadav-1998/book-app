6351b792d7e5b459730146be2d7cfb1214fca77e
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-07-11 18:46:23 +0530

    created AppHeader Component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar is-info">
+        <div class="navbar-brand">
+            <a href="#" class="navbar-item" aria-label="menu" aria-expanded="false">
+                <strong>BookApp</strong>
+            </a>
+            <a class="navbar-burger" role="button" @click="isActive = !isActive">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div class="navbar-menu navbar-end" :class="{'is-active': isActive}">
+            <router-link class="navbar-item" to="/" >Home</router-link>
+            <router-link class="navbar-item" to="/books" >Books</router-link>
+            <router-link class="navbar-item" to="/about" >About</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      isActive: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+.navbar-brand {
+  font-family: "Lobster", cursive;
+  font-size: 25px;
+}
+</style>
