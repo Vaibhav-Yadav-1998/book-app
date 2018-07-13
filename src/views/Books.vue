bf4126fbeb89da2b2dd60df99b6c1f29eca9eebe
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-07-13 19:37:47 +0530

    added card component

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,11 +1,67 @@
 <template>
-    <section id="books">
-        <h1>Books Section</h1>
+    <section id="books" class="has-text-centered">
+        <h1 class="title">Books Section</h1>
+        <div id="grid">
+            <book-card v-for="book in books" :key="book.id"
+            :title="book.title"
+            :author="book.author"
+            :cover="book.cover">
+              <button class="button is-success card-footer-item" @click="edit">
+                  Edit
+              </button>
+              <button class="button is-danger card-footer-item" @click="del">
+                  Delete
+              </button>
+            </book-card>
+        </div>
     </section>
 </template>
 
 <script>
+import Card from "@/components/Card";
+import apiURL from "../../api-config";
+
 export default {
-  name: "Books"
+  name: "Books",
+  components: {
+    "book-card": Card
+  },
+  data() {
+    return {
+      books: ""
+    };
+  },
+  methods: {
+    getBooks() {
+      fetch(apiURL)
+        .then(res => res.json())
+        .then(data => {
+          this.books = data;
+          console.log(this.books);
+        })
+        .catch(err => {
+          console.log(err);
+          alert("sorry something went wrong, try again");
+        });
+    },
+    edit() {
+      console.log("edit");
+    },
+    del() {
+      console.log("delete");
+    }
+  },
+  mounted() {
+    this.getBooks();
+  }
 };
 </script>
+
+<style scoped>
+#grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+</style>
