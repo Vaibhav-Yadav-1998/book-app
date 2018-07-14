34c80eaf17c5049e936aa8f3acafc662c7d1bb99
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-07-15 01:13:56 +0530

    added delete book functionality

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -7,25 +7,25 @@
           <div class="modal-background"></div>
           <div class="modal-content has-background-white">
 
-            <div>
+            <div id="book-form">
               <div class="field">
                 <label for="title" class="label">Title</label>
                 <div class="control">
-                  <input type="text" name="title" class="input" placeholder="title" required v-model="title">
+                  <input type="text" name="title" class="input" placeholder="title" required v-model="title" @keyup.enter="addBook">
                 </div>
               </div>
 
               <div class="field">
                 <label for="author" class="label">Author</label>
                 <div class="control">
-                  <input type="text" name="author" class="input" placeholder="author" required v-model="author">
+                  <input type="text" name="author" class="input" placeholder="author" required v-model="author" @keyup.enter="addBook">
                 </div>
               </div>
 
               <div class="field">
                 <label for="cover" class="label">Cover</label>
                 <div class="control">
-                  <input type="url" name="cover" class="input" placeholder="cover url" v-model="cover">
+                  <input type="url" name="cover" class="input" placeholder="cover url" v-model="cover" @keyup.enter="addBook">
                 </div>
               </div>
 
@@ -41,14 +41,14 @@
         </div>
 
         <div id="grid">
-            <book-card v-for="book in books" :key="book.id"
+            <book-card v-for="book in books" :key="book._id"
             :title="book.title"
             :author="book.author"
             :cover="book.cover">
               <button class="button is-dark card-footer-item" @click="edit">
                   Edit
               </button>
-              <button class="button is-danger card-footer-item" @click="deleteBook()">
+              <button class="button is-danger card-footer-item" @click="deleteBook(book)">
                   Delete
               </button>
             </book-card>
@@ -63,19 +63,21 @@ import axios from "axios";
 
 export default {
   name: "Books",
+
   components: {
     "book-card": Card
   },
+
   data() {
     return {
       books: "",
       isActive: false,
       title: "",
       author: "",
-      cover: "",
-      deleteId: ""
+      cover: ""
     };
   },
+
   methods: {
     getBooks() {
       axios
@@ -89,33 +91,64 @@ export default {
           alert("sorry something went wrong, try again");
         });
     },
-    addBook() {
-      axios
-        .post(apiURL, {
-          title: this.title,
-          author: this.author,
-          cover: this.cover
-        })
-        .then(res => {
-          this.books.push(res);
-        })
-        .catch(err => {
-          console.log(err);
-          alert("sorry something went wrong, try again");
-        });
 
-      this.isActive = false;
+    addBook() {
+      if (
+        this.title.trim().length !== 0 &&
+        this.author.trim().length !== 0 &&
+        this.cover.trim().length !== 0
+      ) {
+        axios
+          .post(apiURL, {
+            title: this.title,
+            author: this.author,
+            cover: this.cover
+          })
+          .then(res => {
+            console.log(res);
+            this.books.push(res.data);
+          })
+          .catch(err => {
+            console.log(err);
+            alert("sorry something went wrong, try again");
+          });
+
+        this.isActive = false;
+        this.title = "";
+        this.author = "";
+        this.cover = "";
+      } else {
+        alert("please fill all the fields");
+      }
     },
+
     edit() {
       console.log("edit");
     },
-    deleteBook() {
-      console.log("delete");
+
+    deleteBook(book) {
+      console.log(book._id);
+      this.books.map(i => {
+        if (i._id === book._id) {
+          axios
+            .delete(`${apiURL}/${i._id}`)
+            .then(res => {
+              console.log(res);
+              this.books.splice(this.books.indexOf(i), 1);
+            })
+            .catch(err => {
+              console.log(err);
+              alert("sorry something went wrong, try again");
+            });
+        }
+      });
     },
+
     close() {
       this.isActive = false;
     }
   },
+
   mounted() {
     this.getBooks();
   }
