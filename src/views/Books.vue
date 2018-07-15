f5e29bd5b72519be08a009dbf2356e4b10239aea
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-07-16 02:23:09 +0530

    added edit book functionality

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -3,6 +3,7 @@
         <h1 class="title">Books Section</h1>
         <button class="button is-success" @click="isActive = true"> + Add Book</button>
 
+        <!-- add book form -->
         <div class="modal" :class="{'is-active': isActive}">
           <div class="modal-background"></div>
           <div class="modal-content has-background-white">
@@ -40,12 +41,51 @@
           <button class="modal-close is-large" @click="close"></button>
         </div>
 
+        <!-- edit book form -->
+        <div class="modal" :class="{'is-active': editActive}">
+          <div class="modal-background"></div>
+          <div class="modal-content has-background-white">
+
+            <div id="edit-form">
+              <div class="field">
+                <label for="title" class="label">Title</label>
+                <div class="control">
+                  <input type="text" name="title" class="input" placeholder="title" required v-model="title" @keyup.enter="editBook">
+                </div>
+              </div>
+
+              <div class="field">
+                <label for="author" class="label">Author</label>
+                <div class="control">
+                  <input type="text" name="author" class="input" placeholder="author" required v-model="author" @keyup.enter="editBook">
+                </div>
+              </div>
+
+              <div class="field">
+                <label for="cover" class="label">Cover</label>
+                <div class="control">
+                  <input type="url" name="cover" class="input" placeholder="cover url" v-model="cover" @keyup.enter="editBook">
+                </div>
+              </div>
+
+              <div class="field">
+                <div class="control">
+                   <button class="button is-primary" @click="editBook">Submit</button>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <button class="modal-close is-large" @click="close"></button>
+        </div>
+
+        <!-- saved books -->
         <div id="grid">
             <book-card v-for="book in books" :key="book._id"
             :title="book.title"
             :author="book.author"
             :cover="book.cover">
-              <button class="button is-dark card-footer-item" @click="edit">
+              <button class="button is-dark card-footer-item" @click="edit(book)">
                   Edit
               </button>
               <button class="button is-danger card-footer-item" @click="deleteBook(book)">
@@ -72,9 +112,11 @@ export default {
     return {
       books: "",
       isActive: false,
+      editActive: false,
       title: "",
       author: "",
-      cover: ""
+      cover: "",
+      editId: ""
     };
   },
 
@@ -122,8 +164,46 @@ export default {
       }
     },
 
-    edit() {
-      console.log("edit");
+    edit(book) {
+      this.title = book.title;
+      this.author = book.author;
+      this.cover = book.cover;
+      this.editId = book._id;
+      this.editActive = true;
+    },
+
+    editBook() {
+      if (
+        this.title.trim().length !== 0 &&
+        this.author.trim().length !== 0 &&
+        this.cover.trim().length !== 0
+      ) {
+        axios
+          .put(`${apiURL}/${this.editId}`, {
+            title: this.title,
+            author: this.author,
+            cover: this.cover
+          })
+          .then(res => {
+            this.books.map(i => {
+              if (i._id === res.data._id) {
+                this.books.splice(this.books.indexOf(i), 1, res.data);
+              }
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            alert("sorry something went wrong, try again");
+          });
+
+        this.title = "";
+        this.author = "";
+        this.cover = "";
+        this.editId = "";
+        this.editActive = false;
+      } else {
+        alert("please fill all fields");
+      }
     },
 
     deleteBook(book) {
@@ -146,6 +226,7 @@ export default {
 
     close() {
       this.isActive = false;
+      this.editActive = false;
     }
   },
 
