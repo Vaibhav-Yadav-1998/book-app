8719914428d8252ed105cdbb47512e30fdd519e4
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-07-17 17:28:07 +0530

    color change

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="welcome" class="hero is-info is-bold is-large">
+  <section id="welcome" class="hero is-dark is-bold is-large">
      <div class="hero-body">
        <div class="container has-text-centered">
          <h1 class="title">Book App</h1>
