c777236bf0f2592d6d51b87de29ad90a13ac7133
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-08-09 03:12:13 +0530

    redesigned homepage

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,14 +1,17 @@
 <template>
-  <section id="welcome" class="hero is-dark is-bold is-large">
-     <div class="hero-body">
-       <div class="container has-text-centered">
-         <h1 class="title">Book App</h1>
-         <h2 class="subtitle">
-           <router-link to="/books">
-             <button class="button">Books</button>
-           </router-link>
-         </h2>
-       </div>
+  <section id="welcome">
+     <div class="logo">
+       <i class="fas fa-book fa-5x"></i>
+     </div>
+     <h1 class="heading">
+       Book App
+     </h1>
+     <div class="book-link">
+       <router-link to="/books">
+         <button class="button is-primary is-large">
+           Books
+         </button>
+       </router-link>
      </div>
   </section>
 </template>
@@ -24,9 +27,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.title {
+#welcome {
+  background-color: #383737;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 95vh;
+}
+
+.logo {
+  color: #00d1b2;
+}
+
+.heading {
   font-size: 3rem;
-  font-family: "Lobster", sans-serif, cursive;
-  margin-bottom: 50px;
+  font-family: "Lobster", Arial, Helvetica, sans-serif;
+  margin: 2rem 0.5rem;
 }
 </style>
